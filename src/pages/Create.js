e35fe4ddc498d3644fe7a71d582b17e8b72f27c6
Daniel Rosel <danalves24@outlook.com>
2021-04-10 17:25:58 +0200

    ing

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -11,16 +11,31 @@ for(var i = 0 ; i < 40; i+=1) {
 
 var ingredients = []
 
+
+
+
+
+
 function ingredient(name) {
 	return `
-	<div class="border-l hover:border-black mx-2">${name}</div>
+	<div onclick="tog(this)" data-slected="false" id="opt" class="border-l hover:border-black mx-2" >${name}</div>
 
 	`
 
 }
 
 
+function getAllSelected() {
+	ingredients = []
+	console.log($("#opt"))
+	var result = document.evaluate("//div[@data-slected='true']", document, null, 0, null), item;
 
+	while (item = result.iterateNext()) {
+		ingredients.push(item.innerText)
+	}
+	localStorage.setItem('ingredients', ingredients.join("~20~"))
+	window.location.pathname = "/compile"
+}
 
 
 
@@ -30,6 +45,7 @@ class Create extends React.Component {
 
 
 	componentDidMount() {
+		localStorage.setItem("ingredients", "")
 		var endpoint = "/api/ingredients/get-all"
 		var settings = {
 	"url": "http://localhost:62964/api/ingredients/get-all",
@@ -54,16 +70,17 @@ class Create extends React.Component {
 			<>
 				<div class="h-screen mx-10">
 				
-					<div class="grid md:grid-cols-2 h-1/2 py-10">
-						<div class="text-center tracking-widest text-3xl flex items-center justify-center h-1/2">
-							<h1 class="my-auto">Create</h1>
+					<div class="grid md:grid-cols-2 h-1/2">
+						<div class="text-center tracking-widest text-3xl flex items-center justify-center h-1/3">
+							<h1 class="my-auto">Select what you have</h1>
 						</div>
-						<div class="h-1/2">
+						<div class="h-1/3">
 							<center class="h-3/4">
 								<InputText placeholder="ingredient" id="ing" color="green" style="w-4/5" />
-								<div class="overflow-scroll h-full" id="list">
+								<div class="overflow-y-scroll h-full shadow" id="list">
 									
-								</div>
+								</div>								
+								<button onClick={getAllSelected} class="tracking-wide my-4 text-2xl bg-green-200 p-2">Create</button>
 							</center>
 						</div>
 					</div>

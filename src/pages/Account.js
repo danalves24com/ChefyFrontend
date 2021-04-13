5f0e945d25cef5300faff01970613a7af21aeecd
Daniel Rosel <danalves24@outlook.com>
2021-04-13 17:48:02 +0200

    full integration done

---
+++ src/pages/Account.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import $ from 'jquery'
+
+
+
+class Acount extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
+
+export default Acount;
